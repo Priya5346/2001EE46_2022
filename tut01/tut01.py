c61a8d5e0e9d2073ef3166dc7a11a32183f5c11b
Priya5346 <112251873+Priya5346@users.noreply.github.com>
2022-09-14 23:22:46 +0530

    calcu;ating other data

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -5,5 +5,11 @@ import numpy as np
 df=pd.read_csv(r'C:\Users\PRIYA RAJ\OneDrive\Documents\GitHub\2001EE46_2022\tut01\octant_input.csv')
 print(df)
 
+df['Uavg'] = df['U'].mean()
+df['Vavg'] = df['V'].mean()
+df['Wavg'] = df['W'].mean()
+df['U-Uavg'] = df['U'] - df['Uavg']
+df['V-Vavg'] = df['V'] - df['Vavg']
+df['W-Wavg'] = df['W'] - df['Wavg']
 
 
