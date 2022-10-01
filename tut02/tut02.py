436b65950cab9e0bf6644b4d0643988e3132eee8
Priya5346 <112251873+Priya5346@users.noreply.github.com>
2022-10-02 00:17:22 +0530

    reading the excel file initially

---
+++ tut02/tut02.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import openpyxl as opxl
+# reading the excel file
+df = pd.read_excel('C:/Users/PRIYA RAJ/OneDrive/Documents/GitHub/CS384_2022/tut02/input_octant_transition_identify.xlsx', 'Sheet1')
+print(df)
