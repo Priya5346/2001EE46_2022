19e58d1fad87a91ff993f214656ebccb28470966
Priya5346 <112251873+Priya5346@users.noreply.github.com>
2022-10-02 00:22:36 +0530

    calculating values of other columns

--- tut02/tut02.py
+++ tut02/tut02.py
@@ -3,4 +3,13 @@ import numpy as np
 import openpyxl as opxl
 # reading the excel file
 df = pd.read_excel('C:/Users/PRIYA RAJ/OneDrive/Documents/GitHub/CS384_2022/tut02/input_octant_transition_identify.xlsx', 'Sheet1')
+
+# finding the mean
+df['Uavg'] = df['U'].mean()
+df['Vavg'] = df['V'].mean()
+df['Wavg'] = df['W'].mean()
+# diffrence of original and mean value
+df['U-Uavg'] = df['U'] - df['Uavg']
+df['V-Vavg'] = df['V'] - df['Vavg']
+df['W-Wavg'] = df['W'] - df['Wavg']
 print(df)
