fa20ee275b040be6e6410ece5a9266076432f569
Priya5346 <112251873+Priya5346@users.noreply.github.com>
2022-10-11 17:50:40 +0530

    reading file using pandas

---
+++ tut03/tut03.py
@@ -0,0 +1,7 @@
+#importing libraries
+import pandas as pd
+import numpy as np
+import openpyxl as opxl
+#reading file
+df = pd.read_excel('C:/Users/PRIYA RAJ/OneDrive/Documents/GitHub/CS384_2022/tut03/input_octant_longest_subsequence.xlsx', 'Sheet1')
+print(df)
