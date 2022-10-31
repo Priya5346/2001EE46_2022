76558d1490838880380f9be6b39cecf690a43cc1
Priya5346 <112251873+Priya5346@users.noreply.github.com>
2022-10-31 23:01:50 +0530

    reading the excel file

---
+++ tut05/tut05.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import openpyxl as opxl
+import scipy.stats as ss
+
+# reading the excel file
+df = pd.read_excel('C:/Users/PRIYA RAJ/OneDrive/Documents/GitHub/CS384_2022/tut05/octant_input.xlsx', 'Sheet1')
+print(df)
