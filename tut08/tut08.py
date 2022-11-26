c71b80d909216b0ddb9cc9f2de79f3a9d61c733f
Priya5346 <112251873+Priya5346@users.noreply.github.com>
2022-11-26 23:02:24 +0530

    india innings

--- tut08/tut08.py
+++ tut08/tut08.py
@@ -132,3 +132,87 @@ print(ind_bowlers)
 print(pak_bats)
 print(out_pak_bat)
 print(pak_byes)
+
+for l in lst_ind:
+    x=l.index(".")
+    over_ind=l[0:x+2]
+
+    temp=l[x+2::].split(",")
+
+    curr_ball=temp[0].split("to") #0 2
+    if f"{curr_ball[0].strip()}" not in pak_bowlers.keys() :
+        pak_bowlers[f"{curr_ball[0].strip()}"]=[1,0,0,0,0,0,0]   #[over0,medan1,runs2,Wickets3, NB4, WD5, ECO6]
+    elif "wide" in temp[1]:
+        pass
+    elif "bye" in temp[1]:
+        if "FOUR" in temp[2]:
+            ind_byes+=4
+        elif "1" in temp[2]:
+            ind_byes+=1
+        elif "2" in temp[2]:
+            ind_byes+=2
+        elif "3" in temp[2]:
+            ind_byes+=3
+        elif "4" in temp[2]:
+            ind_byes+=4
+        elif "5" in temp[2]:
+            ind_byes+=5
+    else:
+        pak_bowlers[f"{curr_ball[0].strip()}"][0]+=1
+    
+    if f"{curr_ball[1].strip()}" not in ind_bats.keys() and temp[1]!="wide":
+        ind_bats[f"{curr_ball[1].strip()}"]=[0,1,0,0,0] #[runs,ball,4s,6s,sr]
+    elif "wide" in temp[1] :
+        pass
+    else:
+        ind_bats[f"{curr_ball[1].strip()}"][1]+=1
+    
+
+    if "out" in temp[1]:
+        pak_bowlers[f"{curr_ball[0].strip()}"][3]+=1
+        
+        if "Bowled" in temp[1].split("!!")[0]:
+            out_ind_bat[f"{curr_ball[1].strip()}"]=("b" + curr_ball[0])
+        elif "Caught" in temp[1].split("!!")[0]:
+            w=(temp[1].split("!!")[0]).split("by")
+            out_ind_bat[f"{curr_ball[1].strip()}"]=("c" + w[1] +" b " + curr_ball[0])
+        elif "Lbw" in temp[1].split("!!")[0]:
+            out_ind_bat[f"{curr_ball[1].strip()}"]=("lbw  b "+curr_ball[0])
+
+    
+    
+    if "no run" in temp[1] or "out" in temp[1] :
+        pak_bowlers[f"{curr_ball[0].strip()}"][2]+=0
+        ind_bats[f"{curr_ball[1].strip()}"][0]+=0
+    elif "1 run" in temp[1]:
+        pak_bowlers[f"{curr_ball[0].strip()}"][2]+=1
+        ind_bats[f"{curr_ball[1].strip()}"][0]+=1
+    elif "2 run" in temp[1]:
+        pak_bowlers[f"{curr_ball[0].strip()}"][2]+=2
+        ind_bats[f"{curr_ball[1].strip()}"][0]+=2
+    elif "3 run" in temp[1]:
+        pak_bowlers[f"{curr_ball[0].strip()}"][2]+=3
+        ind_bats[f"{curr_ball[1].strip()}"][0]+=3
+    elif "4 run" in temp[1]:
+        pak_bowlers[f"{curr_ball[0].strip()}"][2]+=4
+        ind_bats[f"{curr_ball[1].strip()}"][0]+=4
+    elif "FOUR" in temp[1]:
+        pak_bowlers[f"{curr_ball[0].strip()}"][2]+=4
+        ind_bats[f"{curr_ball[1].strip()}"][0]+=4
+        ind_bats[f"{curr_ball[1].strip()}"][2]+=1
+    elif "SIX" in temp[1]:
+        pak_bowlers[f"{curr_ball[0].strip()}"][2]+=6
+        ind_bats[f"{curr_ball[1].strip()}"][0]+=6
+        ind_bats[f"{curr_ball[1].strip()}"][3]+=1
+    elif "wide" in temp[1]:
+        if "wides" in temp[1]:
+            pak_bowlers[f"{curr_ball[0].strip()}"][2]+=int(temp[1][1])
+            pak_bowlers[f"{curr_ball[0].strip()}"][5]+=int(temp[1][1])
+        else:
+            pak_bowlers[f"{curr_ball[0].strip()}"][2]+=1
+            pak_bowlers[f"{curr_ball[0].strip()}"][5]+=1
+
+print(pak_bowlers)
+print(ind_bats)
+print(out_ind_bat)
+print(ind_byes)
