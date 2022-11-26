a7d63e034cf18145feab836162fae1dc22941642
Priya5346 <112251873+Priya5346@users.noreply.github.com>
2022-11-26 22:59:24 +0530

    pakistan innings

--- tut08/tut08.py
+++ tut08/tut08.py
@@ -35,8 +35,7 @@ print(lst_pak)
 wb = openpyxl.Workbook()
 sheet = wb.active
 
-# batting [runs,ball,4s,6s,sr]
-# bowling [over,medan,runs,Wickets, NB, WD, ECO]
+
 ind_fall_of_wickets=0
 pak_fall_of_wickets=0
 out_pak_bat={}
@@ -50,3 +49,86 @@ ind_bowlers_total=0
 pak_byes=0
 ind_byes=0
 
+for l in lst_pak:
+    x=l.index(".")
+    over_pak=l[0:x+2]
+    temp=l[x+2::].split(",")
+    curr_ball=temp[0].split("to") #0 2
+
+    if f"{curr_ball[0].strip()}" not in ind_bowlers.keys() :
+        ind_bowlers[f"{curr_ball[0].strip()}"]=[1,0,0,0,0,0,0]   #[over0,medan1,runs2,Wickets3, NB4, WD5, ECO6]
+    elif "wide" in temp[1]:
+        pass
+    elif "bye" in temp[1]:
+        if "FOUR" in temp[2]:
+            pak_byes+=4
+        elif "1" in temp[2]:
+            pak_byes+=1
+        elif "2" in temp[2]:
+            pak_byes+=2
+        elif "3" in temp[2]:
+            pak_byes+=3
+        elif "4" in temp[2]:
+            pak_byes+=4
+        elif "5" in temp[2]:
+            pak_byes+=5
+
+    else:
+        ind_bowlers[f"{curr_ball[0].strip()}"][0]+=1
+    
+    if f"{curr_ball[1].strip()}" not in pak_bats.keys() and temp[1]!="wide":
+        pak_bats[f"{curr_ball[1].strip()}"]=[0,1,0,0,0] #[runs,ball,4s,6s,sr]
+    elif "wide" in temp[1] :
+        pass
+    else:
+        pak_bats[f"{curr_ball[1].strip()}"][1]+=1
+    
+
+    if "out" in temp[1]:
+        ind_bowlers[f"{curr_ball[0].strip()}"][3]+=1
+        if "Bowled" in temp[1].split("!!")[0]:
+            out_pak_bat[f"{curr_ball[1].strip()}"]=("b" + curr_ball[0])
+        elif "Caught" in temp[1].split("!!")[0]:
+            w=(temp[1].split("!!")[0]).split("by")
+            out_pak_bat[f"{curr_ball[1].strip()}"]=("c" + w[1] +" b " + curr_ball[0])
+        elif "Lbw" in temp[1].split("!!")[0]:
+            out_pak_bat[f"{curr_ball[1].strip()}"]=("lbw  b "+curr_ball[0])
+
+    
+
+    if "no run" in temp[1] or "out" in temp[1] :
+        ind_bowlers[f"{curr_ball[0].strip()}"][2]+=0
+        pak_bats[f"{curr_ball[1].strip()}"][0]+=0
+    elif "1 run" in temp[1]:
+        ind_bowlers[f"{curr_ball[0].strip()}"][2]+=1
+        pak_bats[f"{curr_ball[1].strip()}"][0]+=1
+    elif "2 run" in temp[1]:
+        ind_bowlers[f"{curr_ball[0].strip()}"][2]+=2
+        pak_bats[f"{curr_ball[1].strip()}"][0]+=2
+    elif "3 run" in temp[1]:
+        ind_bowlers[f"{curr_ball[0].strip()}"][2]+=3
+        pak_bats[f"{curr_ball[1].strip()}"][0]+=3
+    elif "4 run" in temp[1]:
+        ind_bowlers[f"{curr_ball[0].strip()}"][2]+=4
+        pak_bats[f"{curr_ball[1].strip()}"][0]+=4
+    elif "FOUR" in temp[1]:
+        ind_bowlers[f"{curr_ball[0].strip()}"][2]+=4
+        pak_bats[f"{curr_ball[1].strip()}"][0]+=4
+        pak_bats[f"{curr_ball[1].strip()}"][2]+=1
+    elif "SIX" in temp[1]:
+        ind_bowlers[f"{curr_ball[0].strip()}"][2]+=6
+        pak_bats[f"{curr_ball[1].strip()}"][0]+=6
+        pak_bats[f"{curr_ball[1].strip()}"][3]+=1
+    elif "wide" in temp[1]:
+        if "wides" in temp[1]:
+            # print(temp[1][1])
+            ind_bowlers[f"{curr_ball[0].strip()}"][2]+=int(temp[1][1])
+            ind_bowlers[f"{curr_ball[0].strip()}"][5]+=int(temp[1][1])
+        else:
+            ind_bowlers[f"{curr_ball[0].strip()}"][2]+=1
+            ind_bowlers[f"{curr_ball[0].strip()}"][5]+=1
+
+print(ind_bowlers)
+print(pak_bats)
+print(out_pak_bat)
+print(pak_byes)
